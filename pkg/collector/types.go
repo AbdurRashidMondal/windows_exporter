472fe029cc565c7a493bb9802905f56ba6b8bb5e
AbdurRashidMondal <abdur.m@gsl.in>
2026-01-28 18:38:48 +0530

    Enhancement : Adding ginprocess in default collector.

--- pkg/collector/types.go
+++ pkg/collector/types.go
@@ -26,7 +26,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-const DefaultCollectors = "cpu,memory,logical_disk,physical_disk,net,os,service,system"
+const DefaultCollectors = "cpu,memory,logical_disk,physical_disk,net,os,service,system,ginprocess"
 
 type Collection struct {
 	collectors    Map
